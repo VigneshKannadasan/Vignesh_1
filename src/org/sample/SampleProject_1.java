b629ba0796e9e87c8142319d939fbb19850ba9c1
VigneshKannadasan <vigneshkannadasan7@gmail.com>
2019-03-29 10:30:41 +0530

    created a base class with launch browser method

---
+++ src/org/sample/SampleProject_1.java
@@ -0,0 +1,12 @@
+package org.sample;
+
+public class SampleProject_1 {
+    public void samplePjt() {
+    	System.out.println("To push the values to repository");
+    }
+	public static void main(String[] args) {
+		SampleProject_1 sp=new SampleProject_1();
+		sp.samplePjt();
+	}
+
+}
